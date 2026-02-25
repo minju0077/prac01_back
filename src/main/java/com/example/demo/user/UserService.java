4ac1b04158a7fbee012e4ec59b4617c1b689309a
mj <qjsalswn@gmail.com>
2026-02-25 11:16:25 +0900

    [Feat] @RestControllerAdvice로 전역 예외 처리

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -22,6 +22,12 @@ public class UserService implements UserDetailsService {
     private final EmailVerifyRepository emailVerifyRepository;
 
     public UserDto.SignupRes signup(UserDto.SignupReq dto) {
+
+        if(userRepository.findByEmail(dto.getEmail()).isPresent()){
+            throw new RuntimeException("중복된 메일입니다.");
+        }
+
+
         User user = dto.toEntity();
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
         userRepository.save(user);
